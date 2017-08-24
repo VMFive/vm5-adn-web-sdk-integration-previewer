15960918169c7714242fdb063dea50aa8a37e642
cades kao <cadeskao@gmail.com>
2017-08-24 17:27:56 +0800

    restructure project

--- init-ad.js
+++ init-ad.js
@@ -1,3 +1,4 @@
 VM5AdSDK.init({
   appKey: '580db14a4a801a2674a56f81',
+  adTypePlugins: [Demo1Plugin, Demo2Plugin],
 });
--- inserter.js
+++ inserter.js
@@ -10,14 +10,12 @@ chrome.runtime.onMessage.addListener(function(msg, sender, response) {
 
 function insertAd(adType, selector) {
   console.warn(adType, selector)
-  var newNode = document.createElement('div');
-  newNode.innerHTML = `
-    <div id="vm5ad-js-sdk" data-mode="fast"></div>
-    <vmfive-ad-unit placement-id="580db15a4a801a2674a56f83" ad-type='${adType}'></vmfive-ad-unit>
-  `;
+  var anchor = createNodeFromString(`<div id="vm5ad-js-sdk" data-mode="fast"></div>`)
+  var adUnit = createNodeFromString(`<vmfive-ad-unit placement-id="580db15a4a801a2674a56f83" ad-type='${adType}' flip-card-front-side-selector="#wrap"></vmfive-ad-unit>`);
 
   var selectorNode = document.querySelector(selector);
-  selectorNode.parentNode.insertBefore(newNode, selectorNode.nextSibling);
+  selectorNode.parentNode.insertBefore(anchor, selectorNode);
+  selectorNode.parentNode.insertBefore(adUnit, selectorNode);
 
   [
     "https://vawpro.vm5apis.com/man.js",
@@ -32,4 +30,10 @@ function insertAd(adType, selector) {
     }
     document.head.appendChild(script);
   });
+}
+
+function createNodeFromString(str) {
+  var node = document.createElement('div');
+  node.innerHTML = str;
+  return node.firstChild;
 }
